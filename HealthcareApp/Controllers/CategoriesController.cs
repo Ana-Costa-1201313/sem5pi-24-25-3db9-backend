f8f66e3542bb930e90a25b2ad2094f7d640934ad
1201313_AnaCosta <1201313@isep.ipp.pt>
2024-10-03 18:29:30 +0100

    Testing pipeline.

--- HealthcareApp/Controllers/CategoriesController.cs
+++ HealthcareApp/Controllers/CategoriesController.cs
@@ -27,7 +27,7 @@ namespace HealthcareApp.Controllers
 
         // GET: api/Categories/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<CategoryDto>> (Guid id)
+        public async Task<ActionResult<CategoryDto>> GetGetById(Guid id)
         {
             var cat = await _service.GetByIdAsync(new CategoryId(id));
 
