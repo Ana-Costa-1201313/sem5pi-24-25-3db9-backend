d58dd205bb4fe6e6dabf7d049f8ccb9bc97221e2
Tiago1191583 <1191583@isep.ipp.pt>
2024-10-06 14:56:11 +0100

    US 5.1.20 - Controller with GET and POST #24

---
+++ HealthcareApp/Controllers/OperationTypesController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+using System.Threading.Tasks;
+using HealthcareApp.Domain.Shared;
+using HealthcareApp.Domain.OperationTypes;
+
+namespace HealthcareApp.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class OperationTypesController : ControllerBase
+    {
+        private readonly OperationTypeService _service;
+
+        public OperationTypesController(OperationTypeService service)
+        {
+            _service = service;
+        }
+
+        // GET: api/OperationTypes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OperationTypeDto>>> GetAll()
+        {
+            return await _service.GetAllAsync();
+        }
+
+        // GET: api/OperationTypes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OperationTypeDto>> GetGetById(Guid id)
+        {
+            var opType = await _service.GetByIdAsync(new OperationTypeId(id));
+
+            if (opType == null)
+            {
+                return NotFound();
+            }
+
+            return opType;
+        }
+
+        // POST: api/OperationTypes
+        [HttpPost]
+        public async Task<ActionResult<OperationTypeDto>> Create(CreatingOperationTypeDto dto)
+        {
+            var opType = await _service.AddAsync(dto);
+
+            return CreatedAtAction(nameof(GetGetById), new { id = opType.Id }, opType);
+        }
+    }
+}
