5682e758bd904c9ca19f53e03d19391f0afb57cf
1201313_AnaCosta <1201313@isep.ipp.pt>
2024-10-12 23:31:15 +0100

    US 5.1.12 - Staff class. #15

---
+++ Backoffice/Domain/Staff/Staff.cs
@@ -0,0 +1,38 @@
+namespace Backoffice.Domain.Staff
+{
+    public class Staff
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string FullName { get; private set; }
+        public int LicenseNumber { get; private set; }
+        public string Email { get; private set; }
+        public string Phone { get; private set; }
+        public string Specialization { get; private set; }
+        public int AvailabilitySlots { get; private set; }
+
+        private Staff()
+        {
+
+        }
+
+        public Staff(string firstName,
+        string lastName,
+        string fullName,
+        int licenseNum,
+        string email,
+        string phone, 
+        string specialization, 
+        int availabilitySlots)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.FullName = fullName;
+            this.LicenseNumber = licenseNum;
+            this.Email = email;
+            this.Phone = phone;
+            this.Specialization = specialization;
+            this.AvailabilitySlots = availabilitySlots;
+        }
+    }
+}
