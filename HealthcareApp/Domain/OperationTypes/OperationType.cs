a85c09bc05e833236a36754a7dae6863698082d4
Tiago1191583 <1191583@isep.ipp.pt>
2024-10-12 14:29:55 +0100

    US 5.1.20 added value objects to OperationType #24

--- HealthcareApp/Domain/OperationTypes/OperationType.cs
+++ HealthcareApp/Domain/OperationTypes/OperationType.cs
@@ -6,9 +6,8 @@ namespace HealthcareApp.Domain.OperationTypes
     public class OperationType : Entity<OperationTypeId>, IAggregateRoot
     {
 
-        public string Description { get; private set; }
         public OperationTypeName Name {get; private set;}
-        public string Duration {get; private set;}
+        public OperationTypeDuration Duration {get; private set;}
         public List<OperationTypeRequiredStaff> RequiredStaff {get; private set;}
 
         public bool Active { get; private set; }
@@ -18,21 +17,20 @@ namespace HealthcareApp.Domain.OperationTypes
             this.Active = true;
         }
 
-        public OperationType(string description, OperationTypeName name, string duration, List<OperationTypeRequiredStaff> requiredStaff)
+        public OperationType(OperationTypeName name, OperationTypeDuration duration, List<OperationTypeRequiredStaff> requiredStaff)
         {
             this.Id = new OperationTypeId(Guid.NewGuid());
-            this.Description = description;
             this.Name = name;
             this.Duration = duration;
             this.RequiredStaff = requiredStaff;
             this.Active = true;
         }
 
-        public void ChangeDescription(string description)
+        public void ChangeDescription(OperationTypeDuration duration)
         {
             if (!this.Active)
                 throw new BusinessRuleValidationException("It is not possible to change the description to an inactive operation type.");
-            this.Description = description;
+            this.Duration = duration;
         }
         public void MarkAsInative()
         {
