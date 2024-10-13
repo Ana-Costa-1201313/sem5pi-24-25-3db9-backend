8a5411048e9d68144608ed81e8e3bb69d1b1823b
Tiago1191583 <1191583@isep.ipp.pt>
2024-10-13 14:46:51 +0100

    US 5.1.20 GetWithDetails #24

--- HealthcareApp/Infraestructure/OperationTypes/OperationTypeRepository.cs
+++ HealthcareApp/Infraestructure/OperationTypes/OperationTypeRepository.cs
@@ -1,17 +1,33 @@
 using HealthcareApp.Domain.Categories;
 using HealthcareApp.Domain.OperationTypes;
 using HealthcareApp.Infraestructure.Shared;
+using Microsoft.EntityFrameworkCore;
 
 namespace HealthcareApp.Infraestructure.Categories
 {
     public class OperationTypeRepository : BaseRepository<OperationType, OperationTypeId>, IOperationTypeRepository
     {
 
+        private readonly BDContext _context;
         public OperationTypeRepository(BDContext context) : base(context.OperationTypes)
         {
-
+            _context = context;
         }
 
+        public async Task<List<OperationType>> GetAllWithDetailsAsync()
+        {
+            return await _context.OperationTypes
+            .Include(rs => rs.RequiredStaff)
+            .ThenInclude(s => s.Specialization)
+            .ToListAsync();      
+        }
 
+        public async Task<OperationType> GetByIdWithDetailsAsync(OperationTypeId id)
+        {
+            return await _context.OperationTypes
+            .Include(rs => rs.RequiredStaff)
+            .ThenInclude(s => s.Specialization)
+            .FirstOrDefaultAsync(rs => rs.Id == id);      
+        }
     }
 }
