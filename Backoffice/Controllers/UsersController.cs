037d5989b4f10259d2b492bc5a3a9a3025da9fe2
1221695 <1221695@isep.ipp.pt>
2024-10-14 17:53:15 +0100

    us5.1.6 - commit branch correction

---
+++ Backoffice/Controllers/UsersController.cs
@@ -0,0 +1,76 @@
+using Microsoft.AspNetCore.Mvc;
+using Backoffice.Domain.Users;
+using Backoffice.Domain.Shared;
+using System.Configuration;
+
+namespace Backoffice.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserService _service;
+
+        public UsersController(UserService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserDto>>> GetAll()
+        {
+            return await _service.GetAllAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserDto>> GetById(Guid id)
+        {
+            var user = await _service.GetByIdAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UserDto>> Create(CreateUserDto dto)
+        {
+            try
+            {
+                var user = await _service.AddAsync(dto);
+
+                return CreatedAtAction(nameof(GetById), new { id = user.Id }, user);
+            }
+            catch (BusinessRuleValidationException e)
+            {
+                return BadRequest(new { Message = e.Message });
+            }
+            catch (ConfigurationErrorsException e)
+            {
+                return StatusCode(500, new { message = e.Message });
+            }
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<ActionResult<UserDto>> UpdatePassword(Guid id, [FromBody] string password)
+        {
+            try
+            {
+                var user = await _service.UpdatePassword(id, password);
+
+                if (user == null)
+                {
+                    return NotFound();
+                }
+                return Ok(user);
+            }
+            catch (BusinessRuleValidationException e)
+            {
+                return BadRequest(new { Message = e.Message });
+            }
+        }
+    }
+}
---
+++ Backoffice/Domain/Users/UserService.cs
@@ -0,0 +1,95 @@
+using Backoffice.Domain.Shared;
+
+namespace Backoffice.Domain.Users
+{
+    public class UserService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        private readonly IUserRepository _repo;
+
+        public UserService(IUnitOfWork unitOfWork, IUserRepository repo)
+        {
+            this._unitOfWork = unitOfWork;
+            this._repo = repo;
+        }
+
+        public async Task<List<UserDto>> GetAllAsync()
+        {
+            var list = await this._repo.GetAllAsync();
+
+            List<UserDto> listDto = list.ConvertAll<UserDto>(u => new UserDto
+            {
+                Id = u.Id.AsGuid(),
+                Role = u.Role,
+                Email = u.Email.ToString(),
+                Password = u.Password
+            });
+
+            return listDto;
+        }
+
+        public async Task<UserDto> GetByIdAsync(Guid id)
+        {
+            var user = await this._repo.GetByIdAsync(new UserId(id));
+
+            if (user == null)
+                return null;
+
+            return new UserDto
+            {
+                Id = user.Id.AsGuid(),
+                Role = user.Role,
+                Email = user.Email.ToString(),
+                Password = user.Password
+            };
+        }
+
+        public async Task<UserDto> AddAsync(CreateUserDto dto)
+        {
+            int mechanographicNum = 0;
+            
+            if (dto.Role != Role.Patient)
+            {
+                mechanographicNum = await this._repo.GetLastMechanographicNumAsync() + 1;
+            }
+
+            var user = new User(dto.Role, dto.Email, mechanographicNum);
+
+            await this._repo.AddAsync(user);
+
+            await this._unitOfWork.CommitAsync();
+
+            return new UserDto
+            {
+                Id = user.Id.AsGuid(),
+                Role = user.Role,
+                Email = user.Email.ToString(),
+                Password = user.Password
+            };
+        }
+
+        public async Task<UserDto> UpdatePassword(Guid id, string password)
+        {
+            var user = await this._repo.GetByIdAsync(new UserId(id));
+
+            if (user == null)
+            {
+                return null;
+            }
+
+            user.ActivateUser(password);
+
+            await this._unitOfWork.CommitAsync();
+
+            return new UserDto
+            {
+                Id = user.Id.AsGuid(),
+                Role = user.Role,
+                Email = user.Email.ToString(),
+                Password = user.Password,
+                Active = user.Active
+            };
+        }
+    }
+}
