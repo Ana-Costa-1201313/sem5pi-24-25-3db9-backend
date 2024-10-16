d1a5a3b434b4fb1c2110488e05e28653494b7152
Tiago1191583 <1191583@isep.ipp.pt>
2024-10-16 21:26:52 +0100

    US 5.1.20 Created OperationType Mapper #24

--- HealthcareApp/Domain/OperationTypes/OperationTypeMapper.cs
+++ HealthcareApp/Domain/OperationTypes/OperationTypeMapper.cs
@@ -1,26 +1,26 @@
 using System;
+using HealthcareApp.Domain.Specializations;
 
 namespace HealthcareApp.Domain.OperationTypes;
 
 public static class OperationTypeMapper
 {
-/*    public static OperationTypeDto ToDto(OperationType operationType)
+    public static OperationTypeDto ToDto(OperationType operationType)
     {
         return new OperationTypeDto
         {
-            Name = operationType.Name.Value, // Assuming OperationTypeName is a value object
-            AnesthesiaPatientPreparationInMinutes = operationType.Duration.AnesthesiaPatientPreparationInMinutes,
-            SurgeryInMinutes = operationType.Duration.SurgeryInMinutes,
-            CleaningInMinutes = operationType.Duration.CleaningInMinutes,
-            RequiredStaff = operationType.RequiredStaff.Select(staff => new OperationTypeRequiredStaffDto
+            Name = operationType.Name.Name,
+            AnesthesiaPatientPreparationInMinutes = (int)operationType.Duration.AnesthesiaPatientPreparationInMinutes.TotalMinutes,
+            SurgeryInMinutes = (int)operationType.Duration.SurgeryInMinutes.TotalMinutes,
+            CleaningInMinutes = (int)operationType.Duration.CleaningInMinutes.TotalMinutes,
+            RequiredStaff = operationType.RequiredStaff.Select(staff => new RequiredStaffDto
             {
-                Specialization = staff.Specialization.Name.Value, // Assuming SpecializationName is a value object
+                Specialization = staff.Specialization.Name.Name,
                 Total = staff.Total
             }).ToList()
         };
     }
 
-    // Mapping DTO back to domain model
     public static OperationType ToDomain(OperationTypeDto dto)
     {
         return new OperationType(
@@ -29,5 +29,15 @@ public static class OperationTypeMapper
             dto.RequiredStaff.Select(staffDto => new OperationTypeRequiredStaff(
                 new Specialization(new SpecializationName(staffDto.Specialization)), staffDto.Total)).ToList()
         );
-    }*/
+    }
+
+    public static OperationType ToDomain(CreatingOperationTypeDto dto)
+    {
+        return new OperationType(
+            new OperationTypeName(dto.Name),
+            new OperationTypeDuration(dto.AnesthesiaPatientPreparationInMinutes, dto.SurgeryInMinutes, dto.CleaningInMinutes),
+            dto.RequiredStaff.Select(staffDto => new OperationTypeRequiredStaff(
+                new Specialization(new SpecializationName(staffDto.Specialization)), staffDto.Total)).ToList()
+        );
+    }
 }
