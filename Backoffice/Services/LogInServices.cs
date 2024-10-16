eb082f96fa834f66a11b9ded7d5c869d165e618e
1221695 <1221695@isep.ipp.pt>
2024-10-16 11:40:10 +0100

    US5.1.6 - checkTokenValidaty

--- Backoffice/Services/LogInServices.cs
+++ Backoffice/Services/LogInServices.cs
@@ -4,21 +4,24 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
 using Backoffice.Domain.Users;
+using System.Text;
 
 namespace Backoffice.Services
 {
     public class LogInServices
     {
-        protected string validateUrl = "validate";
+        protected string validateUrl = "Login/validate";
 
         private readonly IConfiguration _iconfig;
         private readonly IUserRepository _userRepository;
+        private readonly HttpClient _httpClient;
 
 
-        public LogInServices(IConfiguration iConfig, IUserRepository userRepository)
+        public LogInServices(IConfiguration iConfig, IUserRepository userRepository, HttpClient httpClient)
         {
             this._iconfig = iConfig;
             this._userRepository = userRepository;
+            this._httpClient = httpClient;
         }
 
         public async Task<bool> CheckUserCredentials1(string username, string password)
@@ -41,6 +44,34 @@ namespace Backoffice.Services
             loginDTO.role = user.Role.ToString();
 
             return loginDTO;
+        }
+
+        public async Task<LoginDTO> checkTokenValidaty(LoginDTO loginDTO)
+        {
+            if (loginDTO.jwt == "")
+            {
+                return null;
+            }
+            try
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, _iconfig["Auth:urlLocal"] + validateUrl);
+
+                request.Headers.Add("Authorization", $"Bearer {loginDTO.jwt}");
+
+                HttpResponseMessage responseMessage = await _httpClient.SendAsync(request);
+
+                responseMessage.EnsureSuccessStatusCode();
+
+                string responseBody = await responseMessage.Content.ReadAsStringAsync();
+
+                LoginDTO resultLoginDTO = JsonSerializer.Deserialize<LoginDTO>(responseBody);
+
+                return resultLoginDTO;
+            }
+            catch(Exception e)
+            {
+                throw new 
+            }
         }
     }
 }
