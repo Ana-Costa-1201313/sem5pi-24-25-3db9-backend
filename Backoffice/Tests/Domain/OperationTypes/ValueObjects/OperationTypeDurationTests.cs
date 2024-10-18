4a011b52f4a1b69146463764e4178d57e27410fe
Tiago1191583 <1191583@isep.ipp.pt>
2024-10-18 15:04:16 +0100

    US 5.1.20 Tests for VO's of OperationType entity #24

--- Backoffice/Tests/Domain/OperationTypes/ValueObjects/OperationTypeDurationTests.cs
+++ Backoffice/Tests/Domain/OperationTypes/ValueObjects/OperationTypeDurationTests.cs
@@ -89,4 +89,56 @@ public class OperationTypeDurationTests
 
         Assert.Equal("Error: The cleaning duration must be more than 0 minutes.", cleaningException.Message);
     }
+
+
+    //After teoric class
+
+    [Theory]
+    [InlineData(100, 20, 20)]
+    [InlineData(50, 5, 55)]
+    [InlineData(1000, 1000, 1000)]
+    [InlineData(1, 1, 1)]
+    public void WhenPassingCorrectData_ThenOperationTypeDurationIsInstantiated(int anesthesiaPatientPreparationInMinutes, int surgeryInMinutes, int cleaningInMinutes)
+    {
+        new OperationTypeDuration(anesthesiaPatientPreparationInMinutes, surgeryInMinutes, cleaningInMinutes);
+    }
+
+    [Theory]
+    [InlineData(0, 5, 55)]
+    [InlineData(-1, 1000, 1000)]
+    [InlineData(-1000, 1, 1)]
+    public void WhenPassingInvalidAnesthesiaPatientPreparationInMinutes_ThenThrowsException(int anesthesiaPatientPreparationInMinutes, int surgeryInMinutes, int cleaningInMinutes)
+    {
+        var ex = Assert.Throws<BusinessRuleValidationException>(() =>
+
+            new OperationTypeDuration(anesthesiaPatientPreparationInMinutes, surgeryInMinutes, cleaningInMinutes)
+        );
+        Assert.Equal("Error: The anesthesia/preparation duration must be more than 0 minutes.", ex.Message);
+    }
+
+    [Theory]
+    [InlineData(50, 0, 55)]
+    [InlineData(1000, -1, 1000)]
+    [InlineData(1, -1000, 1)]
+    public void WhenPassingInvalidSurgeryInMinutes_ThenThrowsException(int anesthesiaPatientPreparationInMinutes, int surgeryInMinutes, int cleaningInMinutes)
+    {
+        var ex = Assert.Throws<BusinessRuleValidationException>(() =>
+
+            new OperationTypeDuration(anesthesiaPatientPreparationInMinutes, surgeryInMinutes, cleaningInMinutes)
+        );
+        Assert.Equal("Error: The surgery duration must be more than 0 minutes.", ex.Message);
+    }
+
+    [Theory]
+    [InlineData(50, 5, 0)]
+    [InlineData(1000, 1000, -1)]
+    [InlineData(1, 1, -1000)]
+    public void WhenPassingInvalidCleaningInMinutes_ThenThrowsException(int anesthesiaPatientPreparationInMinutes, int surgeryInMinutes, int cleaningInMinutes)
+    {
+        var ex = Assert.Throws<BusinessRuleValidationException>(() =>
+
+            new OperationTypeDuration(anesthesiaPatientPreparationInMinutes, surgeryInMinutes, cleaningInMinutes)
+        );
+        Assert.Equal("Error: The cleaning duration must be more than 0 minutes.", ex.Message);
+    }
 }
