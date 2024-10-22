2786105ae28c035457ba5f6913d7bd8af2ec3189
Luis Reis <1210998@isep.ipp.pt>
2024-10-22 17:14:58 +0100

    Added a Method to get a list of patients by various attributs #12

--- Backoffice/Infraestructure/Patients/PatientRepository.cs
+++ Backoffice/Infraestructure/Patients/PatientRepository.cs
@@ -12,6 +12,25 @@ namespace Backoffice.Infraestructure.Patients
         {
             _context = context;
         }
+
+        public async Task<List<Patient>> SearchPatientsAsync(string name, string email,DateTime? dateOfBirth
+        , int? medicalRecordNumber)
+        {
+            var query = _context.Patients.AsQueryable();
+
+            if(!string.IsNullOrEmpty(name))
+                query = query.Where(p => p.FullName.Contains(name));
+            if(!string.IsNullOrEmpty(email))
+                query = query.Where(p => p.Email._Email == email);
+            if(dateOfBirth.HasValue)
+                query = query.Where(p => p.DateOfBirth == dateOfBirth);
+            if(medicalRecordNumber.HasValue)
+                query = query.Where(p=> p.MedicalRecordNumber == medicalRecordNumber);
+
+                return await query.ToListAsync();
+        }
+
+
         public async Task<Patient> GetPatientByEmailAsync(Email email){
             return await _context.Patients.Where(p => p.Email == email).FirstOrDefaultAsync();
         }
