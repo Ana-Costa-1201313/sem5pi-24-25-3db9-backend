8b41b6e033e7dc41671c33094195ba4c626415b2
1201313_AnaCosta <1201313@isep.ipp.pt>
2024-10-23 10:18:41 +0100

    US 5.1.13 - Staff Name tests. #16

--- Backoffice/Tests/Domain/Staffs/StaffTests.cs
+++ Backoffice/Tests/Domain/Staffs/StaffTests.cs
@@ -55,6 +55,169 @@ namespace Backoffice.Tests
             Assert.Equal(Email._Email, staff.Email._Email);
         }
 
+
+        [Fact]
+        public void NullFirstName()
+        {
+            List<string> AvailabilitySlots = new List<string>();
+            AvailabilitySlots.Add("2024 - 10 - 10T12: 00:00 / 2024 - 10 - 11T15: 00:00");
+            AvailabilitySlots.Add("2024 - 10 - 14T12: 00:00 / 2024 - 10 - 19T15: 00:00");
+
+            CreateStaffDto dto = new CreateStaffDto
+            {
+                FirstName = null,
+                LastName = "costa",
+                FullName = "ana costa",
+                LicenseNumber = 1,
+                Phone = "999999999",
+                Specialization = "spec",
+                AvailabilitySlots = AvailabilitySlots,
+                Role = Role.Nurse,
+                RecruitmentYear = 2024
+            };
+
+
+            int MecNumSequence = 1;
+
+            var exception = Assert.Throws<BusinessRuleValidationException>(() => new Staff(dto, MecNumSequence));
+            Assert.Equal("Error: The staff must have a first name!", exception.Message);
+        }
+
+        [Fact]
+        public void EmptyFirstName()
+        {
+            List<string> AvailabilitySlots = new List<string>();
+            AvailabilitySlots.Add("2024 - 10 - 10T12: 00:00 / 2024 - 10 - 11T15: 00:00");
+            AvailabilitySlots.Add("2024 - 10 - 14T12: 00:00 / 2024 - 10 - 19T15: 00:00");
+
+            CreateStaffDto dto = new CreateStaffDto
+            {
+                FirstName = "",
+                LastName = "costa",
+                FullName = "ana costa",
+                LicenseNumber = 1,
+                Phone = "999999999",
+                Specialization = "spec",
+                AvailabilitySlots = AvailabilitySlots,
+                Role = Role.Nurse,
+                RecruitmentYear = 2024
+            };
+
+
+            int MecNumSequence = 1;
+
+            var exception = Assert.Throws<BusinessRuleValidationException>(() => new Staff(dto, MecNumSequence));
+            Assert.Equal("Error: The staff must have a first name!", exception.Message);
+        }
+
+        [Fact]
+        public void NullLastName()
+        {
+            List<string> AvailabilitySlots = new List<string>();
+            AvailabilitySlots.Add("2024 - 10 - 10T12: 00:00 / 2024 - 10 - 11T15: 00:00");
+            AvailabilitySlots.Add("2024 - 10 - 14T12: 00:00 / 2024 - 10 - 19T15: 00:00");
+
+            CreateStaffDto dto = new CreateStaffDto
+            {
+                FirstName = "ana",
+                LastName = null,
+                FullName = "ana costa",
+                LicenseNumber = 1,
+                Phone = "999999999",
+                Specialization = "spec",
+                AvailabilitySlots = AvailabilitySlots,
+                Role = Role.Nurse,
+                RecruitmentYear = 2024
+            };
+
+
+            int MecNumSequence = 1;
+
+            var exception = Assert.Throws<BusinessRuleValidationException>(() => new Staff(dto, MecNumSequence));
+            Assert.Equal("Error: The staff must have a last name!", exception.Message);
+        }
+
+        [Fact]
+        public void EmptyLastName()
+        {
+            List<string> AvailabilitySlots = new List<string>();
+            AvailabilitySlots.Add("2024 - 10 - 10T12: 00:00 / 2024 - 10 - 11T15: 00:00");
+            AvailabilitySlots.Add("2024 - 10 - 14T12: 00:00 / 2024 - 10 - 19T15: 00:00");
+
+            CreateStaffDto dto = new CreateStaffDto
+            {
+                FirstName = "ana",
+                LastName = "",
+                FullName = "ana costa",
+                LicenseNumber = 1,
+                Phone = "999999999",
+                Specialization = "spec",
+                AvailabilitySlots = AvailabilitySlots,
+                Role = Role.Nurse,
+                RecruitmentYear = 2024
+            };
+
+
+            int MecNumSequence = 1;
+
+            var exception = Assert.Throws<BusinessRuleValidationException>(() => new Staff(dto, MecNumSequence));
+            Assert.Equal("Error: The staff must have a last name!", exception.Message);
+        }
+
+        [Fact]
+        public void NullFullName()
+        {
+            List<string> AvailabilitySlots = new List<string>();
+            AvailabilitySlots.Add("2024 - 10 - 10T12: 00:00 / 2024 - 10 - 11T15: 00:00");
+            AvailabilitySlots.Add("2024 - 10 - 14T12: 00:00 / 2024 - 10 - 19T15: 00:00");
+
+            CreateStaffDto dto = new CreateStaffDto
+            {
+                FirstName = "ana",
+                LastName = "costa",
+                FullName = null,
+                LicenseNumber = 1,
+                Phone = "999999999",
+                Specialization = "spec",
+                AvailabilitySlots = AvailabilitySlots,
+                Role = Role.Nurse,
+                RecruitmentYear = 2024
+            };
+
+
+            int MecNumSequence = 1;
+
+            var exception = Assert.Throws<BusinessRuleValidationException>(() => new Staff(dto, MecNumSequence));
+            Assert.Equal("Error: The staff must have a full name!", exception.Message);
+        }
+
+        [Fact]
+        public void EmptyFullName()
+        {
+            List<string> AvailabilitySlots = new List<string>();
+            AvailabilitySlots.Add("2024 - 10 - 10T12: 00:00 / 2024 - 10 - 11T15: 00:00");
+            AvailabilitySlots.Add("2024 - 10 - 14T12: 00:00 / 2024 - 10 - 19T15: 00:00");
+
+            CreateStaffDto dto = new CreateStaffDto
+            {
+                FirstName = "ana",
+                LastName = "costa",
+                FullName = "",
+                LicenseNumber = 1,
+                Phone = "999999999",
+                Specialization = "spec",
+                AvailabilitySlots = AvailabilitySlots,
+                Role = Role.Nurse,
+                RecruitmentYear = 2024
+            };
+
+
+            int MecNumSequence = 1;
+
+            var exception = Assert.Throws<BusinessRuleValidationException>(() => new Staff(dto, MecNumSequence));
+            Assert.Equal("Error: The staff must have a full name!", exception.Message);
+        }
+
         [Fact]
         public void SmallPhone()
         {
@@ -108,6 +271,35 @@ namespace Backoffice.Tests
             var exception = Assert.Throws<BusinessRuleValidationException>(() => new Staff(dto, MecNumSequence));
             Assert.Equal("Error: The phone number is invalid!", exception.Message);
         }
+
+        [Fact]
+        public void WrongRole()
+        {
+            List<string> AvailabilitySlots = new List<string>();
+            AvailabilitySlots.Add("2024 - 10 - 10T12: 00:00 / 2024 - 10 - 11T15: 00:00");
+            AvailabilitySlots.Add("2024 - 10 - 14T12: 00:00 / 2024 - 10 - 19T15: 00:00");
+
+            CreateStaffDto dto = new CreateStaffDto
+            {
+                FirstName = "ana",
+                LastName = "costa",
+                FullName = "ana costa",
+                LicenseNumber = 1,
+                Phone = "999999999",
+                Specialization = "spec",
+                AvailabilitySlots = AvailabilitySlots,
+                Role = Role.Patient,
+                RecruitmentYear = 2024
+            };
+
+
+            int MecNumSequence = 1;
+
+            var exception = Assert.Throws<BusinessRuleValidationException>(() => new Staff(dto, MecNumSequence));
+            Assert.Equal("Error: The staff role must be one of the following: Admin, Doctor, Nurse or Tech!", exception.Message);
+        }
+
+
 
         [Fact]
         public void InvalidSlotFormat()
@@ -188,6 +380,6 @@ namespace Backoffice.Tests
             var exception = Assert.Throws<BusinessRuleValidationException>(() => new Staff(dto, MecNumSequence));
             Assert.Equal("Error: The year must be bigger than zero!", exception.Message);
         }
-       
+
     }
 }
