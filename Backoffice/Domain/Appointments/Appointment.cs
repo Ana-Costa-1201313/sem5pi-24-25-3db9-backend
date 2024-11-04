b070cd525a29bb88694e9e780e201e91f056a900
VicenteCardoso1180664 <1180664@isep.ipp.pt>
2024-11-04 11:12:39 +0000

    Added constructors to Appointment and SurgeryRoom, added TimeSlot.

--- Backoffice/Domain/Appointments/Appointment.cs
+++ Backoffice/Domain/Appointments/Appointment.cs
@@ -1,4 +1,7 @@
 using System;
+using Azure;
+using Newtonsoft.Json;
+using Backoffice.Domain.Shared;
 using Backoffice.Domain.Appointments.ValueObjects;
 using Backoffice.Domain.OperationRequests;
 using Backoffice.Domain.Staffs;
@@ -6,13 +9,40 @@ using Backoffice.Domain.SurgeryRooms;
 
 namespace Backoffice.Domain.Appointments
 {
-    public class Appointment
+    public class Appointment : Entity<AppointmentId>, IAggregateRoot
     {
-        public AppointmentId Id { get; set; }
+        //public AppointmentId Id { get; set; }
         public OperationRequestId OperationRequestId { get; set; }
+        public OperationRequest OperationRequest { get; set; }
         public List<StaffId> StaffIds { get; set; }
+        public List<Staff> Staff { get; set; }
         public SurgeryRoomId SurgeryRoomId { get; set; }
+        public SurgeryRoom SurgeryRoom { get; set; }
         public DateTime DateTime { get; set; }
         public AppointmentStatus Status { get; set; }
+
+        private Appointment() { }
+
+        public Appointment(OperationRequest operationRequest, List<Staff> staff, SurgeryRoom surgeryRoom, DateTime dateTime)
+        {
+            this.Id = new AppointmentId(Guid.NewGuid());
+
+            if (operationRequest == null) throw new BusinessRuleValidationException("OperationRequest can't be null.");
+            this.OperationRequest = operationRequest;
+            this.OperationRequestId = operationRequest.Id;
+
+            if (staff == null || !staff.Any()) throw new BusinessRuleValidationException("Staff can't be null or empty.");
+            this.Staff = staff;
+            this.StaffIds = staff.Select(x => x.Id).ToList();
+
+            if (surgeryRoom == null) throw new BusinessRuleValidationException("SurgeryRoom can't be null.");
+            this.SurgeryRoom = surgeryRoom;
+            this.SurgeryRoomId = surgeryRoom.Id;
+
+            if (dateTime == default) throw new BusinessRuleValidationException("DateTime can't be default value.");
+            this.DateTime = dateTime;
+            
+            this.Status = AppointmentStatus.Scheduled;
+        }
     }
 }
--- Backoffice/Domain/SurgeryRooms/SurgeryRoom.cs
+++ Backoffice/Domain/SurgeryRooms/SurgeryRoom.cs
@@ -1,19 +1,35 @@
+using Backoffice.Domain.Shared;
 using Backoffice.Domain.SurgeryRooms.ValueObjects;
 
 namespace Backoffice.Domain.SurgeryRooms
 {
-    public class SurgeryRoom
+    public class SurgeryRoom : Entity<SurgeryRoomId>, IAggregateRoot
     {
-        public SurgeryRoomId SurgeryRoomId { get; set; }
+        //public SurgeryRoomId SurgeryRoomId { get; set; }
         public Int32 RoomNumber { get; set; }
         public SurgeryRoomType SurgeryRoomType { get; set; }
         public Int16 Capacity { get; set; }
         public SurgeryRoomStatus SurgeryRoomStatus { get; set; }
-        public List<DateTime> MaintenanceSlots { get; set; }
+        public List<TimeSlot> MaintenanceSlots { get; set; }
 
-        public SurgeryRoom()
+        public SurgeryRoom(){}
+
+        public SurgeryRoom(Int32 roomNumber, SurgeryRoomType surgeryRoomType, Int16 capacity, List<TimeSlot> maintenanceSlots)
         {
-            MaintenanceSlots = new List<DateTime>();
+            this.Id = new SurgeryRoomId(Guid.NewGuid());
+
+            if (roomNumber == default || roomNumber <= 0) throw new BusinessRuleValidationException("RoomNumber can't be 0 nor negative.");
+            this.RoomNumber = roomNumber;
+
+            if (surgeryRoomType == SurgeryRoomType.Null) throw new BusinessRuleValidationException("SurgeryRoomType can't be null.");
+            this.SurgeryRoomType = surgeryRoomType;
+
+            if (capacity == default || capacity <= 0) throw new BusinessRuleValidationException("Capacity can't be 0 nor negative.");
+            this.Capacity = capacity;
+
+            //if (maintenanceSlots == null || !maintenanceSlots.Any()) throw new BusinessRuleValidationException("MaintenanceSlots can't be null or empty.");
+            this.SurgeryRoomStatus = SurgeryRoomStatus.Available;
+            this.MaintenanceSlots = maintenanceSlots;
         }
     }
 }
